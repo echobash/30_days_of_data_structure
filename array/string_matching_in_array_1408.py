ab763ed6ab7e44414c81d4609c6b529de73c6117
Ali Anwar <greenywish@gmail.com>
2025-01-07 15:05:27 +0530

    Solve Leetcode Question 1408. String Matching in an Array

--- array/string_matching_in_array_1408.py
+++ array/string_matching_in_array_1408.py
@@ -4,13 +4,16 @@ from typing import List
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         n = len(words)
-        result = set()
-        words = sorted(words, key=len)
+        result = []
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(n):
+                if i == j:
+                    continue
                 if words[i] in words[j]:
-                    result.add(words[i])
-        return list(result)
+                    result.append(words[i])
+                    # as soon as we found that this string is subset of atleast one other string,break and come out and look for next element otherwise duplicates may be caught
+                    break
+        return result
 
 
 
@@ -21,7 +24,6 @@ print(words, sol.stringMatching(words))
 
 words = ["leetcode","et","code"]
 print(words, sol.stringMatching(words))
-
 
 words = ["blue","green","bu"]
 print(words, sol.stringMatching(words))
