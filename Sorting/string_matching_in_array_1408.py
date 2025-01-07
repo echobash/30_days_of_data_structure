8589a95dd3bf4c36832325dfbd97a59f88d0cb75
Ali Anwar <greenywish@gmail.com>
2025-01-07 14:35:32 +0530

    Solve Leetcode Question 1408. String Matching in an Array

---
+++ Sorting/string_matching_in_array_1408.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        n = len(words)
+        result = set()
+        words = sorted(words, key=len)
+        for i in range(n):
+            for j in range(i+1, n):
+                if words[i] in words[j]:
+                    result.add(words[i])
+        return list(result)
+
+
+
+sol = Solution()
+
+words = ["mass","as","hero","superhero"]
+print(words, sol.stringMatching(words))
+
+words = ["leetcode","et","code"]
+print(words, sol.stringMatching(words))
+
+
+words = ["blue","green","bu"]
+print(words, sol.stringMatching(words))
+
