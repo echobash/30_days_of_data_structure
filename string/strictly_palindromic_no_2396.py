354481dc0d3ceb570335fc48c84309e0c93c88c0
Ali Anwar <greenywish@gmail.com>
2025-01-11 23:58:45 +0530

    Solve Leetcode Question 2396. Strictly Palindromic Number

---
+++ string/strictly_palindromic_no_2396.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+
+sol = Solution()
+
+n = 9
+print(n, sol.isStrictlyPalindromic(n))
+
+n = 4
+print(n, sol.isStrictlyPalindromic(n))
