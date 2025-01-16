44d41b737ecfd3f05de74d987e7b757def106a37
Ali Anwar <greenywish@gmail.com>
2025-01-17 04:42:37 +0530

    Implement Binary Search

---
+++ binarysearch/binary_search_iterative.py
@@ -0,0 +1,23 @@
+class Solution:
+    def binary_search(self, left, right):
+
+        target = 12
+        while left <= right:
+            mid = (right + left) // 2
+            # print(left,right,mid)
+            if a[mid] == target:
+                return mid
+            elif a[mid] > target:
+                # Go to left
+                right = mid - 1
+            else:
+                # Go to right
+                left = mid + 1
+        return -1
+
+
+a = [3, 4, 6, 7, 9, 12, 16, 17]
+sol = Solution()
+n = len(a)
+left, right = 0, n - 1
+print(sol.binary_search(left, right))
