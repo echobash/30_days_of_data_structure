f3a3a882e093f131b56a527c2ac5124942bd274b
Ali Anwar <greenywish@gmail.com>
2025-01-20 23:26:38 +0530

    Solve Leetcode Question 2843. Count Symmetric Integers

--- binarysearch/first_occurence_in_a_sorted_array.py
+++ binarysearch/first_occurence_in_a_sorted_array.py
@@ -5,7 +5,6 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
         left, right = 0, n - 1
-        ans = -1
 
         while left <= right:
             mid = (left + right) // 2
