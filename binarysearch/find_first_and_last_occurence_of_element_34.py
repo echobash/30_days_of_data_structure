905ed818f6c27aa05a57f15f9c9286eae578246a
Ali Anwar <greenywish@gmail.com>
2025-01-21 19:23:48 +0530

    Solve Leetcode Question 34. Find First and Last Position of Element in Sorted Array

--- binarysearch/find_first_and_last_occurence_of_element_34.py
+++ binarysearch/find_first_and_last_occurence_of_element_34.py
@@ -2,43 +2,37 @@ from typing import List
 
 
 class Solution:
-    def findFirstOccurence(self, position,nums,target):
-        if position == 0:
-            return 0
-        while(nums[position] == target):
-            if position == 0:
-                return 0
-            position -= 1
-        return position + 1
+    def findFirstOccurence(self, a, n, target):
+        left, right = 0, n - 1
+        ans = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if a[mid] == target:
+                ans = mid
+                right = mid - 1
+            elif a[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
 
-    def findLastOccurence(self, position,nums,target):
-        if position == len(nums)-1:
-            return len(nums)-1
-        while(nums[position] == target):
-            if position == len(nums)-1:
-                return len(nums)-1
-            position += 1
-        return position - 1
+    def findLastOccurence(self, a, n, target):
+        left, right = 0, n - 1
+        ans = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if a[mid] == target:
+                ans = mid
+                left = mid + 1
+            elif a[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
 
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        left, right = 0, n-1
-
-        while left <= right:
-            mid = (left + right)//2
-
-            if nums[mid] == target:
-                first = self.findFirstOccurence(mid,nums,target)
-                last = self.findLastOccurence(mid,nums,target)
-                return [first, last]
-            elif nums[mid] > target:
-                # Move to left
-                right = mid - 1
-            else:
-                # Move to right
-                left = mid + 1
-        return [-1,-1]
-
+        return [self.findFirstOccurence(nums, n, target), self.findLastOccurence(nums, n, target)]
 
 
 sol = Solution()
