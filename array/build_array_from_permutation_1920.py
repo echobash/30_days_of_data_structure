df25a805692541dd09f5183ab41302c06b4ddd9d
Ali Anwar <greenywish@gmail.com>
2025-02-04 15:00:52 +0530

    Solve Leetcode Question 1800. Maximum Ascending Subarray Sum using bruteforce

--- array/build_array_from_permutation_1920.py
+++ array/build_array_from_permutation_1920.py
@@ -2,18 +2,36 @@ from typing import List
 
 
 class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        max_sum = 0
         n = len(nums)
-        ans = [0]*n
-        for i in range(n):
-            ans[i] = nums[nums[i]]
-        return ans
+        if n == 1:
+            return nums[0]
+        for i in range(n-1):
+            sum = nums[i]
+            for j in range(i+1, n):
+                if nums[j] > nums[j-1]:
+                    sum += nums[j]
+                else:
+                    break
+            max_sum = max(max_sum,sum)
+        return max_sum
+
 
 
 sol = Solution()
 
-nums = [0,2,1,5,3,4]
-print(nums, sol.buildArray(nums))
+nums = [10,20,30,5,10,50]
+print(f" {nums = } | {sol.maxAscendingSum(nums) = }")
 
-nums = [5,0,1,2,3,4]
-print(nums, sol.buildArray(nums))
+nums = [10,20,30,40,50]
+print(f" {nums = } | {sol.maxAscendingSum(nums) = }")
+
+nums = [50,40,30,20,10]
+print(f" {nums = } | {sol.maxAscendingSum(nums) = }")
+
+nums = [12,17,15,13,10,11,12]
+print(f" {nums = } | {sol.maxAscendingSum(nums) = }")
+
+nums = [12]
+print(f" {nums = } | {sol.maxAscendingSum(nums) = }")
