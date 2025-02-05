7e6b819e2d0092de87f362abcc2f5824d16b0c73
Ali Anwar <greenywish@gmail.com>
2025-02-05 18:48:00 +0530

    Use no extra space

--- string/check_if_one_character_swap_can_make_two_strings_equal_1790_no_extra_space.py
+++ string/check_if_one_character_swap_can_make_two_strings_equal_1790_no_extra_space.py
@@ -6,9 +6,6 @@ class Solution:
         if s1 == s2:
             return True
 
-        if Counter(s1) != Counter(s2):
-            return False
-
         mismatch_character_count = 0
         first_mismatching_char = ''
         second_mismatching_char = ''
@@ -33,17 +30,17 @@ sol = Solution()
 
 s1 = "bank"
 s2 = "kanb"
-print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+print(f" {s1 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
 
 s1 = "attack"
 s2 = "defend"
-print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+print(f" {s1 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
 
 s1 = "kelb"
 s2 = "kelb"
-print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+print(f" {s1 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
 
 s1 = "caa"
 s2 = "aaz"
-print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+print(f" {s1 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
 
