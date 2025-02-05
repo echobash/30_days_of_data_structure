4515a03a046646a0eb250da063240fa8ca894b4b
Ali Anwar <greenywish@gmail.com>
2025-02-05 18:45:02 +0530

    Solve Leetcode Question 1790. Check if One String Swap Can Make Strings Equal Without extra space

---
+++ string/check_if_one_character_swap_can_make_two_strings_equal_1790_no_extra_space.py
@@ -0,0 +1,49 @@
+from collections import Counter
+
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+
+        if Counter(s1) != Counter(s2):
+            return False
+
+        mismatch_character_count = 0
+        first_mismatching_char = ''
+        second_mismatching_char = ''
+        n = len(s1)
+        for i in range(n):
+            if s1[i] != s2[i]:
+                mismatch_character_count += 1
+                if mismatch_character_count == 1:
+                    first_mismatching_char = s1[i]
+                    second_mismatching_char = s2[i]
+                else:
+                    if s1[i] != second_mismatching_char or s2[i] != first_mismatching_char:
+                        return False
+                if mismatch_character_count > 2:
+                    return False
+
+        return mismatch_character_count == 2
+
+
+
+sol = Solution()
+
+s1 = "bank"
+s2 = "kanb"
+print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+
+s1 = "attack"
+s2 = "defend"
+print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+
+s1 = "kelb"
+s2 = "kelb"
+print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+
+s1 = "caa"
+s2 = "aaz"
+print(f" {s2 =} | {s2 =} | {sol.areAlmostEqual(s1, s2) =}")
+
