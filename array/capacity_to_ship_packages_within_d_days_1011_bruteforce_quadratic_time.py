1be41197fc86f2572d4947862cf18f48c1a68d88
Ali Anwar <greenywish@gmail.com>
2025-02-06 12:32:34 +0530

    Bruteforce Solution Quadratic time capacity_to_ship_packages_within_d_days_1011

--- array/capacity_to_ship_packages_within_d_days_1011_bruteforce_quadratic_time.py
+++ array/capacity_to_ship_packages_within_d_days_1011_bruteforce_quadratic_time.py
@@ -3,43 +3,40 @@ from typing import List
 
 
 class Solution:
-    def get_no_of_days(self,w,cap):
-        sum = 0
-        d = 0
-        i = 0
-        while i < len(w):
-            sum += w[i]
-            if sum <= cap:
-                i += 1
-                if i == len(w)-1:
-                    d += 1
-                continue
-            else:
-                d += 1
-                sum = 0
-        return d
-
-    def shipWithinDays(self, w: List[int], days: int) -> int:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        # We know that the ans will definitely lie b/w max(w) and sum(w)
         if days == 1:
-            return sum(w)
-        left, right = max(w), sum(w)+1
-        for cap in range(left, right):
-            days_required = self.get_no_of_days(w,cap)
-            if days_required <= days:
-                return cap
+            return sum(weights)
 
+        left, right = max(weights), sum(weights) + 1
+
+        for min_cap in range(left, right):
+            current_sum = 0
+            day = 1
+            for weight in weights:
+                if current_sum + weight <= min_cap:
+                    current_sum += weight
+                else:
+                    day += 1
+                    current_sum = weight
+            if day <= days:
+                return min_cap
 
 
 sol = Solution()
 
 weights = [3,2,2,4,1,4]
 days = 3
-print(f"{weights = } | {sol.shipWithinDays(weights, days) = }")
+print(f"{weights = }  | {days = } | {sol.shipWithinDays(weights, days) = }")
 
 weights = [5,4,4,5,2,3,4,5,6]
 days = 5
-print(f"{weights = } | {sol.shipWithinDays(weights, days) = }")
+print(f"{weights = }  | {days = } | {sol.shipWithinDays(weights, days) = }")
 
 weights = [1,2,3,4,5,6,7,8,9,10]
 days = 5
-print(f"{weights = } | {sol.shipWithinDays(weights, days) = }")
+print(f"{weights = }  | {days = } | {sol.shipWithinDays(weights, days) = }")
+
+weights = [1,2,3,1,1]
+days = 4
+print(f"{weights = }  | {days = } | {sol.shipWithinDays(weights, days) = }")
