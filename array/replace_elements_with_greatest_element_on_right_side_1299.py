4d12abf812110af49e632c4cb0586eba3e8785dd
Ali Anwar <greenywish@gmail.com>
2025-02-07 01:03:35 +0530

    Solve Leetcode Question 1726. Tuple with Same Product

--- array/replace_elements_with_greatest_element_on_right_side_1299.py
+++ array/replace_elements_with_greatest_element_on_right_side_1299.py
@@ -1,28 +1,36 @@
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        n = len(arr)
-        result = [0] * n
-        right_max = -1
-        result[n-1] = right_max
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        product_count_mapping = defaultdict(int)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                product_count_mapping[nums[i] * nums[j]] += 1
 
-        # Traverse the array from the reverse side
-        for i in range(n-2,-1,-1):
-            #compare right_max and next array element
-            if  arr[i+1] > right_max:
-                result[i] = arr[i+1]
-                right_max = arr[i+1]
-            else:
-                result[i] = right_max
-        return result
+        total_no_of_pair_of_product_tuples = 0
+        for product, count in product_count_mapping.items():
+            if count > 1:
+                total_no_of_pair_of_product_tuples += (count * (count - 1) // 2)
+        # Say count = 4 => there are 4 pairs who have equal products i.e 4 tuples.
+        # but we need only 2 tuples at a time only as per the questions
+        # So we will have count-C-2 pair of tuples ie 4C2 pair of tuples
+        # ie. count(count-1)//2
+
+        # Two pair of tuples will have 8 permutations
+        return 8 * total_no_of_pair_of_product_tuples
 
 
 sol = Solution()
 
-arr = [17,18,5,4,6,1]
-print(arr, sol.replaceElements(arr))
+nums = [2, 3, 4, 6]
+print(f"{nums = } | {sol.tupleSameProduct(nums) = }")
 
-arr = [400]
-print(arr, sol.replaceElements(arr))
+nums = [1,2,4,5,10]
+print(f"{nums = } | {sol.tupleSameProduct(nums) = }")
+
+nums = [2,3,4,6,8,12]
+print(f"{nums = } | {sol.tupleSameProduct(nums) = }")
+
