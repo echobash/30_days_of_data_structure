83b5c79ae42a63f5d9efb3b1a42f6caedeed7e89
Ali Anwar <greenywish@gmail.com>
2025-02-16 23:49:11 +0530

    Remove Debugs

--- array/kth_missing_positive_no_from_sorted_array_1539.py
+++ array/kth_missing_positive_no_from_sorted_array_1539.py
@@ -3,27 +3,29 @@ from typing import List
 
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        current_set = set(arr)
-        count = 0
-        element = 1
-        while count < k:
-            if element not in current_set:
-                count += 1
-            element += 1
-        return element-1
+        # arr = [2,3,4,7,11]
+        n = len(arr)
+        missing_count = 0
+        set_of_given_no = set(arr)
+        # the missing no will always lie b/w 1, len(arr) + k
+        for i in range(1, n+k+1):
+            if i not in set_of_given_no:
+                missing_count += 1
+            if missing_count == k:
+                return i
 
 
 sol = Solution()
 
 arr = [2,3,4,7,11]
 k = 5
-print(f"{arr= } {sol.findKthPositive(arr,k)= }")
+print(f"{arr= } | {k = } {sol.findKthPositive(arr,k)= }")
 
 arr = [1,2,3,4]
 k = 2
-print(f"{arr= } {sol.findKthPositive(arr,k)= }")
+print(f"{arr= } | {k = } {sol.findKthPositive(arr,k)= }")
 
 arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 k = 999
-print(f"{arr= } {sol.findKthPositive(arr,k)= }")
+print(f"{arr= } | {k = } {sol.findKthPositive(arr,k)= }")
 
