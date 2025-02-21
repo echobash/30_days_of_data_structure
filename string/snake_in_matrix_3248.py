e73ad5c4262d4a1f1c9b8aa3f90ae42a43013061
Ali Anwar <greenywish@gmail.com>
2025-02-21 10:54:11 +0530

    Solve Leetcode Question 3248. Snake in Matrix

---
+++ string/snake_in_matrix_3248.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        """
+        i,j   #origin
+        i,j+1 #RIGHT
+        i,j-1 #LEFT
+        i-1,j #UP
+        i+1,j #DOWN
+        """
+        final_position = 0
+        i = j = 0
+        for command in commands:
+            if command == "RIGHT":
+                j = j + 1
+            elif command == "LEFT":
+                j = j - 1
+            elif command == "UP":
+                i = i - 1
+            else:
+                i = i + 1
+            final_position = i * n + j
+        return final_position
+
+
+sol = Solution()
+
+n = 2
+commands = ["RIGHT","DOWN"]
+print(f"{n = } {commands = } {sol.finalPositionOfSnake(n, commands) = }")
+
+n = 3
+commands = ["DOWN","RIGHT","UP"]
+print(f"{n = } {commands = } {sol.finalPositionOfSnake(n, commands) = }")
