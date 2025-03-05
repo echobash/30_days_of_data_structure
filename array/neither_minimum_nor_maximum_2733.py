968ab25e0e6cb726fa8587c10873a04790e39d75
Ali Anwar <greenywish@gmail.com>
2025-03-05 20:34:13 +0530

    Solve Leetcode Question 2733. Neither Minimum nor Maximum

---
+++ array/neither_minimum_nor_maximum_2733.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        min_no = min(nums)
+        max_no = max(nums)
+
+        for num in nums:
+            if num != min_no and num != max_no:
+                return num
+
+        return -1
+
+
+sol = Solution()
+
+nums = [3,2,1,4]
+print(f"{nums = } {sol.findNonMinOrMax(nums) = }")
