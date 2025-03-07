3b88474f069b4c51691c87648382269ea9bbd73a
Ali Anwar <greenywish@gmail.com>
2025-03-07 23:39:47 +0530

    Add Missing Edge case

--- Maths/is_prime_optimized.py
+++ Maths/is_prime_optimized.py
@@ -8,6 +8,9 @@ def isPrime(N):
     if(N%2 == 0):
         return "Not Prime"
 
+    if(N%3 == 0):
+        return "Not Prime"
+
     for i in range(5, int(N**0.5)+1, 2):
         if(N%i ==0):
             return "Not Prime"
@@ -15,3 +18,4 @@ def isPrime(N):
     return "Prime"
 
 print(isPrime(14537))
+print(isPrime(9))
