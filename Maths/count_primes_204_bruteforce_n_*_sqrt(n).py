f051a95202571902cae6b1b618e16af7b7707a1d
Ali Anwar <greenywish@gmail.com>
2025-03-07 23:52:05 +0530

    Add Comments

--- Maths/count_primes_204_bruteforce_n_*_sqrt(n).py
+++ Maths/count_primes_204_bruteforce_n_*_sqrt(n).py
@@ -21,17 +21,11 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         prime_count = 0
 
-        is_last_number_prime = self.is_prime(n)
-
-        for j in range(1, n + 1):
+        # We have to check prime less than n i.e without including n
+        for j in range(1, n):
             if self.is_prime(j):
                 prime_count += 1
-        """
-        if say given n = 3, then we know that 2, 3 are both primes but we don't want to include 3 as the question says we want primes strictly less than n so return prime_count - 1
-        But say given n = 4, we will definitely include 2,3 so just return prime_count
-        return prime_count if is_last_number_prime == False else prime_count - 1
-        """
-        return prime_count if is_last_number_prime == False else prime_count - 1
+        return prime_count
 
 
 sol = Solution()
