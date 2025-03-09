8f7645358d676cfc4a22b5ee0acb3c02e30c4fd4
Ali Anwar <greenywish@gmail.com>
2025-03-10 01:45:24 +0530

    Solve Leetcode Question 2523. Closest Prime Numbers in Range in N Log(Log n) Using Sieve of Erastothenes

--- Maths/closest_prime_no_in_ranges_2523.py
+++ Maths/closest_prime_no_in_ranges_2523.py
@@ -2,37 +2,53 @@ from typing import List
 
 
 class Solution:
-    def sieve(self, n):
-        is_prime = [True] * (n + 1)
-        is_prime[0] = is_prime[1] = False
+    def sieve_of_eratosthenes(self, n: int):
+        result = []
+        if n < 2:
+            return result
 
+        all_elements = [True] * n
         for i in range(2, int(n ** 0.5) + 1):
-            if is_prime[i]:
-                for j in range(i * i, n + 1, i):
-                    is_prime[j] = False
-        return is_prime
+            if all_elements[i]:
+                for j in range(i * i, n, i):
+                    all_elements[j] = False
 
-    def closestPrimes(self, left: int, right: int) -> List[int]:
-        is_prime = self.sieve(right)
-        prev = -1
-        result = [-1, -1]
-        min_diff = float('inf')
-
-        for i in range(left, right + 1):
-            if is_prime[i]:
-                if prev != -1 and (i - prev) < min_diff:
-                    min_diff = i - prev
-                    result = [prev, i]
-                prev = i
+        for k in range(2, n):
+            if all_elements[k]:
+                result.append(k)
 
         return result
 
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        all_primes_upto_right = self.sieve_of_eratosthenes(right + 1)
+        min_diff = float('inf')
+        result = [-1, -1]
+
+        l = len(all_primes_upto_right)
+        for i in range(l - 1):
+            if all_primes_upto_right[i] < left:
+                continue
+
+            if all_primes_upto_right[i + 1] - all_primes_upto_right[i] < min_diff:
+                min_diff = all_primes_upto_right[i + 1] - all_primes_upto_right[i]
+                result[0] = all_primes_upto_right[i]
+                result[1] = all_primes_upto_right[i + 1]
+
+        return result
 
 
 sol = Solution()
 
 left = 10
 right = 19
+print(f" {left = } {right = } {sol.closestPrimes(left, right) = }")
+
+left = 12
+right = 24
+print(f" {left = } {right = } {sol.closestPrimes(left, right) = }")
+
+left = 19
+right = 31
 print(f" {left = } {right = } {sol.closestPrimes(left, right) = }")
 
 left = 4
