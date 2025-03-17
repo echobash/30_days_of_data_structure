10c2f1605a8fa7f7789058c876902af026164ffc
Ali Anwar <greenywish@gmail.com>
2025-03-17 16:35:25 +0530

    Solve Leetcode Question 2206. Divide Array Into Equal Pairs using Counter

--- hashing/divide_array_into_equal_parts_2206.py
+++ hashing/divide_array_into_equal_parts_2206.py
@@ -1,19 +1,15 @@
-from collections import defaultdict
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        count_mapping = defaultdict(int)
-        # Store all the nums in the dictionary
-        for num in nums:
-            count_mapping[num] += 1
-
-        # Traverse the dictionary and check if there is atleast one such case where...
+        num_frequency = Counter(nums)
+        # Traverse the array and check if there is at least one such case where...
         # ... the count is odd
 
-        for num, count in count_mapping.items():
-            if count % 2 == 1:
+        for frequency in num_frequency.values():
+            if frequency % 2 == 1:
                 return False
         return True
 
