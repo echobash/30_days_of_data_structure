30a44154bb6e594b111d0e00da7b088e254699e7
Ali Anwar <greenywish@gmail.com>
2025-04-17 23:12:19 +0530

    Solve Leetcode Question 2176. Count Equal and Divisible Pairs in an Array Bruteforce

---
+++ array/count_equal_and_divisible_pairs_in_an_array_2176_bruteforce.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        total_count = 0
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] == nums[j] and (i * j) % k == 0:
+                    total_count += 1
+        return total_count
+
+
+sol = Solution()
+
+nums = [3,1,2,2,2,1,3]
+k = 2
+print(f"{nums = } {k = } {sol.countPairs(nums, k) = }")
+
+nums = [1,2,3,4]
+k = 1
+print(f"{nums = } {k = } {sol.countPairs(nums, k) = }")
+
+nums = [3,1,2,12,2,12,3]
+k = 2
+print(f"{nums = } {k = } {sol.countPairs(nums, k) = }")
