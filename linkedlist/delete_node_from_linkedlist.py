691d6cf3cdf0e767fb5f8edfbb94fcfe64939ef4
Ali Anwar <greenywish@gmail.com>
2025-04-19 17:27:28 +0530

    Delete head of linked List

---
+++ linkedlist/delete_node_from_linkedlist.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self,data ):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.count_nodes = 0
+
+
+    def printLL(self):
+        temp = self.head
+        print(f"LinkedList = ",end="")
+        while temp is not None:
+            print(f"{temp.data}->",end="")
+            temp = temp.next
+        print(None)
+        print("-------------------------------------------------")
+
+    def getLength(self):
+        self.count_nodes = 0
+        temp = self.head
+        while temp is not None:
+            self.count_nodes += 1
+            temp = temp.next
+        return self.count_nodes
+
+    def deleteHead(self):
+        if not self.head:
+            print("Can't delete. The LinkedList is already empty")
+            return
+        else:
+            print(f"Deleted head node with data = {self.head.data}")
+            self.head = self.head.next
+
+    def append(self,data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+            self.temp = self.head
+        else:
+            self.temp.next = node
+            self.temp = self.temp.next
+
+
+ll = LinkedList()
+ll.append(12)
+ll.append(9)
+ll.append(15)
+ll.append(17)
+ll.append(20)
+ll.append(44)
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
+
+ll.deleteHead()
+ll.printLL()
