8c748a8fe8ad61fa6848d2f4cc67a2cba10f4326
Ali Anwar <greenywish@gmail.com>
2025-04-24 00:17:18 +0530

    Solve Leetcode Question 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ string/check_if_a_word_occurs_as_a_prefix_of_any_word_of_the_sentence_1455.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+        for i, word in enumerate(words):
+            if word.startswith(searchWord):
+                return i + 1
+        return -1
+
+
+sol = Solution()
+
+sentence = "i love eating burger"
+searchWord = "burg"
+print(f"{sentence = }  {searchWord = } {sol.isPrefixOfWord(sentence, searchWord) = }")
+
+sentence = "this problem is an easy problem"
+searchWord = "pro"
+print(f"{sentence = }  {searchWord = } {sol.isPrefixOfWord(sentence, searchWord) = }")
+
+sentence = "i am tired"
+searchWord = "you"
+print(f"{sentence = }  {searchWord = } {sol.isPrefixOfWord(sentence, searchWord) = }")
