358b6791310e40a9271ac94ab5dd8acaa220da2b
Ali Anwar <greenywish@gmail.com>
2025-04-30 13:01:32 +0530

    Solve Leetcode Question 1295. Find Numbers with Even Number of Digits

---
+++ Maths/find_no_with_even_no_of_digits_1295.py
@@ -0,0 +1,25 @@
+from math import log10, floor
+from typing import List
+
+
+class Solution:
+    def hasEvenNoOfDigits(self,number):
+        return (floor(log10(number)) + 1) % 2 == 0
+
+    def findNumbers(self, nums: List[int]) -> int:
+        numbers_with_even_no_of_digits = 0
+        for num in nums:
+            if not self.hasEvenNoOfDigits(num):
+                continue
+            numbers_with_even_no_of_digits += 1
+        return numbers_with_even_no_of_digits
+
+
+sol = Solution()
+
+nums = [12,345,2,6,7896]
+print(f"{nums = } {sol.findNumbers(nums) = }")
+
+nums = [555,901,482,1771]
+print(f"{nums = } {sol.findNumbers(nums) = }")
+
