7f54dc8d5aee44eaa3db60e0a59f360f6e84bc39
Ali Anwar <greenywish@gmail.com>
2025-05-04 13:29:11 +0530

    Solve Leetcode Question 1128. Number of Equivalent Domino Pairs

---
+++ hashing/no_of_equivalent_dominoes_pairs_1128_hashing_bruteforce.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        freq_count = dict()
+        sorted_dominoes = []
+        for domino in dominoes:
+            sorted_dominoes.append(sorted(domino))
+
+        for domino in sorted_dominoes:
+            if str(domino[0])+str(domino[1]) in freq_count:
+                freq_count[str(domino[0])+str(domino[1])] += 1
+            else:
+                freq_count[str(domino[0])+str(domino[1])] = 1
+
+        total_count =0
+        for dom, freq in freq_count.items():
+            if freq > 1:
+                total_count += (freq * (freq-1)//2)
+
+        return total_count
+
+        # Check which element will be the same either on top or down i.e count>= n
+
+
+solution = Solution()
+
+dominoes = [[1,2],[2,1],[3,4],[5,6]]
+print(f"{dominoes = } {solution.numEquivDominoPairs(dominoes) = }")
+
+dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2],[4,3],[3,4],[3,3],[4,3]]
+print(f"{dominoes = } {solution.numEquivDominoPairs(dominoes) = }")
+
+dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+print(f"{dominoes = } {solution.numEquivDominoPairs(dominoes) = }")
