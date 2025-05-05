2e0a3f7a24b55ac0c6798bec71da7e84c2a82f59
Ali Anwar <greenywish@gmail.com>
2025-05-05 19:32:06 +0530

    Solve Leetcode Question 234. Palindrome Linked List

---
+++ linkedlist/leetcode/palindrome_linked_list_234.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        node_list = []
+
+        if not head:
+            return False
+
+        if not head.next:
+            return True
+
+        # Store nodes in a list as it is
+        temp = head
+        while temp is not None:
+            node_list.append(temp.val)
+            temp = temp.next
+
+        # Reverse the LL
+        temp = head
+        prev = None
+        while temp is not None:
+            future = temp.next
+            temp.next = prev
+            prev = temp
+            temp = future
+        head = prev
+
+        # Store the reversed nodes in new list
+        reversed_node_list = []
+        temp = head
+        while temp is not None:
+            reversed_node_list.append(temp.val)
+            temp = temp.next
+
+        return reversed_node_list == node_list
