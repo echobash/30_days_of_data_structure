6492f2c8ddbb88fed718b85753fa24d38fd743ba
Ali Anwar <greenywish@gmail.com>
2025-05-09 20:51:38 +0530

    Clean code

--- linkedlist/leetcode/odd_even_linked_list_328_optimized_O(1)_space.py
+++ linkedlist/leetcode/odd_even_linked_list_328_optimized_O(1)_space.py
@@ -2,12 +2,11 @@
 from typing import Optional
 
 
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         i = 0
@@ -15,8 +14,8 @@ class Solution:
         if not head:
             return None
         prev = temp
-        even_ll = ListNode(0)
-        root = even_ll
+        odd_ll = ListNode(0)
+        root = odd_ll
 
         while temp is not None:
             if i % 2 == 0:
@@ -31,5 +30,5 @@ class Solution:
 
         # IMPORTANT: terminate even list
         root.next = None
-        prev.next = even_ll.next
+        prev.next = odd_ll.next
         return head
