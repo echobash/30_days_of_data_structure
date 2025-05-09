ed1fd66f71c31d125ad54c3fa3e59a207583f1e6
Ali Anwar <greenywish@gmail.com>
2025-05-09 20:48:02 +0530

    Solve Leetcode Question 328. Odd Even Linked List in optimized O(1) space

---
+++ linkedlist/leetcode/odd_even_linked_list_328_optimized_O(1)_space.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        i = 0
+        temp = head
+        if not head:
+            return None
+        prev = temp
+        even_ll = ListNode(0)
+        root = even_ll
+
+        while temp is not None:
+            if i % 2 == 0:
+                prev = temp
+                temp = temp.next
+            else:
+                root.next = temp
+                root = temp
+                temp = temp.next
+                prev.next = temp
+            i += 1
+
+        # IMPORTANT: terminate even list
+        root.next = None
+        prev.next = even_ll.next
+        return head
