ec1d1cce20242ed363c529b14262cacbc2d005a0
Ali Anwar <greenywish@gmail.com>
2025-05-25 23:30:33 +0530

    Solve Leetcode Question 347. Top K Frequent Elements using optimized stored tuples in heap instead of the int

--- heap/top_k_frequent_elements_347_optimized_with_tupled_stored_in_heap.py
+++ heap/top_k_frequent_elements_347_optimized_with_tupled_stored_in_heap.py
@@ -6,24 +6,17 @@ import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_mapping = Counter(nums)
-        rev_freq_mapping = defaultdict(list)
-
-        for num, freq in freq_mapping.items():
-            rev_freq_mapping[freq].append(num)
-
-        heap = []
-        for num in freq_mapping.values():
-            if len(heap) < k:
-                heapq.heappush(heap, num)
-            elif num > heap[0]:
-                heapq.heapreplace(heap, num)
 
         result = []
-        unique_heap_freq = list(set(heap))
-        count = 0
-        for freq in unique_heap_freq:
-            for val in rev_freq_mapping[freq]:
-                result.append(val)
+        heap = []
+        for num, freq in freq_mapping.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (freq, num))
+            elif freq > heap[0][0]:
+                heapq.heapreplace(heap, (freq, num))
+
+        for freq, num in heap:
+            result.append(num)
         return result
 
 
