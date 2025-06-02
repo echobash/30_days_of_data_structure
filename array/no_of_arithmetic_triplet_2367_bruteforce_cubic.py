43e3a9e7c6206380f36ecb7528b997785df69b0a
Ali Anwar <greenywish@gmail.com>
2025-06-03 03:07:28 +0530

    Solve Leetcode Question 2367. Number of Arithmetic Triplets bruteforce cubic

---
+++ array/no_of_arithmetic_triplet_2367_bruteforce_cubic.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        n = len(nums)
+        result_set = set()
+        for i in range(n):
+            for j in range(i+1,n):
+                for k in range(j+1,n):
+                    if nums[j] - nums[i] == nums[k] - nums[j] == diff:
+                        result_set.add(tuple(sorted([nums[i], nums[j],nums[k]])))
+        return len(result_set)
+
+
+sol = Solution()
+
+nums = [0,1,4,6,7,10]
+diff = 3
+print(f" {nums = } | {diff = } | {sol.arithmeticTriplets(nums,diff) = }")
+
+nums = [4,5,6,7,8,9]
+diff = 2
+print(f" {nums = } | {diff = } | {sol.arithmeticTriplets(nums,diff) = }")
