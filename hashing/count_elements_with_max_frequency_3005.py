51ecc9932e78ded113f26c2c1da95216732c5070
Ali Anwar <greenywish@gmail.com>
2025-06-11 23:35:21 +0530

    Solve Leetcode Question 3005. Count Elements With Maximum Frequency

---
+++ hashing/count_elements_with_max_frequency_3005.py
@@ -0,0 +1,29 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        num_freq = Counter(nums)
+        max_freq = 0
+        count_of_elements = 0
+
+        for freq in num_freq.values():
+            max_freq = max(max_freq, freq)
+
+        for element, freq in num_freq.items():
+            if freq == max_freq:
+                count_of_elements += freq
+        return count_of_elements
+
+
+sol = Solution()
+
+nums = [1,2,2,3,1,4]
+print(f" {nums = } | {sol.maxFrequencyElements(nums) = }")
+
+nums = [1,2,3,4,5]
+print(f" {nums = } | {sol.maxFrequencyElements(nums) = }")
+
+nums = [1,2,3,4,3,5,3]
+print(f" {nums = } | {sol.maxFrequencyElements(nums) = }")
