52844856ec3d3d0e25f9044c93782f2e877a9a31
Ali Anwar <greenywish@gmail.com>
2025-06-17 06:35:47 +0530

    Solve Leetcode Question 3. Longest Substring Without Repeating Characters

---
+++ sliding_window/longest_substring_without_repeating_character_3.py
@@ -0,0 +1,50 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        max_length = 0
+        curr_length = 0
+        left = 0
+        char_set = set()
+
+        # Start iterating s from i = 0 to n
+        # if s[i] not in set - curr_length = len(set), update max_length
+        # else: - while s[i] in set, remove s[left] from set and set curr_length = len(set), left +=1 and
+        # add s[i] in set
+
+        for i in range(n):
+            if s[i] not in char_set:
+                char_set.add(s[i])
+                curr_length = len(char_set)
+                max_length = max(max_length, curr_length)
+            else:
+                while s[i] in char_set:
+                    char_set.remove(s[left])
+                    left += 1
+                char_set.add(s[i])
+                curr_length = len(char_set)
+                max_length = max(max_length, curr_length)
+        return max_length
+
+
+sol = Solution()
+
+s = "abcabcbb"
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
+
+s = "bbbbb"
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
+
+s = "pwwkew"
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
+
+s = "abccbcbb"
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
+
+s = "abc dbcbb"
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
+
+s = " "
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
+
+s = "        "
+print(f"{s = } {sol.lengthOfLongestSubstring(s) = }")
