868fbe164b6b965634489ca53defc271ac5fbea8
Ali Anwar <greenywish@gmail.com>
2025-06-24 23:22:53 +0530

    Solve Leetcode Question 2390. Removing Stars From a String

---
+++ stack/removing_stars_from_string_2390.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        char_stack = []
+        for char in s:
+            if char != '*':
+                char_stack.append(char)
+            else:
+                char_stack.pop()
+        return "".join(char_stack)
+
+
+sol = Solution()
+
+s = "leet**cod*e"
+print(f" {s = } {sol.removeStars(s) =}")
+
+s = "erase*****"
+print(f" {s = } {sol.removeStars(s) =}")
+
+s = "e*"
+print(f" {s = } {sol.removeStars(s) =}")
