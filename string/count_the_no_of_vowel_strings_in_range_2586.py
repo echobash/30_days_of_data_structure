05a3b742cce8b98c3bf709442584a6f14ec8f9c4
Ali Anwar <greenywish@gmail.com>
2025-07-09 00:01:51 +0530

    Refactor Code

--- string/count_the_no_of_vowel_strings_in_range_2586.py
+++ string/count_the_no_of_vowel_strings_in_range_2586.py
@@ -4,8 +4,9 @@ from typing import List
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
         count = 0
+        vowels = set('aeiouAEIOU')
         for i in range(left, right + 1):
-            if words[i][0] in {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'} and words[i][-1] in  {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}:
+            if words[i][0] in vowels and words[i][-1] in vowels:
                 count += 1
         return count
 
