8ef2fba3f26af9a4360614a4d3b72a35842965a8
Ali Anwar <greenywish@gmail.com>
2025-07-09 23:58:35 +0530

    Solve Leetcode Question 1837. Sum of Digits in Base K

---
+++ Maths/sum_of_digits_in_base_k_1837.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        original = n
+        ans = ""
+        sum_dig = 0
+        while n > 0:
+            rem = n % k
+            n = n//k
+            sum_dig += rem
+        return sum_dig
+
+
+sol = Solution()
+
+n = 34
+k = 6
+print(f"{n = } {k = } {sol.sumBase(n, k) = }")
