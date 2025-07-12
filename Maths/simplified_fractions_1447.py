89697aa795e37bda5f2f9d7f85c751f321ed4ce7
Ali Anwar <greenywish@gmail.com>
2025-07-13 00:13:12 +0530

    Refactor Code

--- Maths/simplified_fractions_1447.py
+++ Maths/simplified_fractions_1447.py
@@ -6,8 +6,8 @@ class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
         result = []
         for i in range(1, n+1):
-            for j in range(1, n+1):
-                if gcd(i, j) != 1 or i >= j:
+            for j in range(i+1, n+1):
+                if gcd(i, j) != 1:
                     continue
                 result.append(f"{i}/{j}")
         return result
