a7dcf76641222e2a7bef2f6132062dffb777c395
Ali Anwar <greenywish@gmail.com>
2025-07-18 00:07:00 +0530

    Solve Leetcode Question 7. Reverse Integer for 32 bit environment optimized

---
+++ Maths/reverse_integer_7_valid_for_32_bit_optimized.py
@@ -0,0 +1,33 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_negative = False
+
+        if 0 <= x <= 9:
+            return x
+
+        if x < 0:
+            is_negative = True
+            x *= -1
+
+        final_number = 0
+        while x != 0:
+            if final_number > (2 ** 31 - 1) // 10 or final_number == (2 ** 31 - 1) // 10 and x % 10 > 7:
+                return 0
+            final_number = final_number * 10 + x % 10
+            x //= 10
+        return -1 * final_number if is_negative else final_number
+
+
+sol = Solution()
+
+x = 123
+print(f"{x = } {sol.reverse(x) = }")
+
+x = -123
+print(f"{x = } {sol.reverse(x) = }")
+
+x = 120
+print(f"{x = } {sol.reverse(x) = }")
+
+x = 8
+print(f"{x = } {sol.reverse(x) = }")
