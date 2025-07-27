488f985c80d80b93adf691ce44458cfb0ceafdb4
Ali Anwar <greenywish@gmail.com>
2025-07-28 01:18:07 +0530

    Calculate Factorial using recursion

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,8 +1,23 @@
-def factorial(n):
-    if(n == 0):
-        return 1
+class Factorial:
+    def getFactorial(self, n):
+        if n == 0:
+            return 1
+        return n * self.getFactorial(n-1)
 
-    return n * factorial(n-1)
+
+factorial = Factorial()
+
+n = 10
+print(f"{n = } {factorial.getFactorial(n) = }")
+
+n = 8
+print(f"{n = } {factorial.getFactorial(n) = }")
+
+n = 6
+print(f"{n = } {factorial.getFactorial(n) = }")
 
 n = 0
-print("the factorial of",n,"is",factorial(n),end="")
+print(f"{n = } {factorial.getFactorial(n) = }")
+#
+# f(x) = x.(x-1)! if x >=1
+#     = 1 if x == 0
