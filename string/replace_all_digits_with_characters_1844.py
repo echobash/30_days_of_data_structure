67418839ab1cc1a1c739ec2acb80f5a7d51e1cce
Ali Anwar <greenywish@gmail.com>
2025-08-29 02:26:07 +0530

    Solve Leetcode Question 1844. Replace All Digits with Characters

---
+++ string/replace_all_digits_with_characters_1844.py
@@ -0,0 +1,26 @@
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        current_char = ''
+        result = ""
+        n = len(s)
+        i = 0
+        while i <= n-1:
+            current_char = s[i]
+            i += 1
+            result += current_char
+            if i!= n:
+                result += chr(ord(current_char)+int(s[i]))
+                i += 1
+        return "".join(result)
+
+
+sol = Solution()
+
+s = "a1c1e1"
+print(f"{s = }  {sol.replaceDigits(s) = }")
+
+s = "a1b2c3d4e"
+print(f"{s = }  {sol.replaceDigits(s) = }")
+
+s = "a1b2c3d4"
+print(f"{s = }  {sol.replaceDigits(s) = }")
