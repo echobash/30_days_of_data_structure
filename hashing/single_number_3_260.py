ecbf91fa757dab6e7b9a87489c95348b22dad8f0
Ali Anwar <greenywish@gmail.com>
2025-09-16 02:53:21 +0530

    Solve Leetcode Question 260. Single Number III

---
+++ hashing/single_number_3_260.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        num_freq = Counter(nums)
+        result = []
+        for num,freq in num_freq.items():
+            if freq == 1:
+                result.append(num)
+        return result
+
+
+sol = Solution()
+
+nums = [1,2,1,3,2,5]
+print(f"{nums = } {sol.singleNumber(nums) = }")
+
+nums = [-1,0]
+print(f"{nums = } {sol.singleNumber(nums) = }")
+
+nums = [0,1]
+print(f"{nums = } {sol.singleNumber(nums) = }")
