ca1222734538da16c80def9dbdfe23251c3f103f
Ali Anwar <greenywish@gmail.com>
2025-09-24 01:21:42 +0530

    Solve Leetcode Question 1720. Decode XORed Array

---
+++ array/decode_xored_array_1720.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        result = [first]
+        for num in encoded:
+            result.append(num ^ first)
+            first = num ^ first
+        return result
+
+
+sol = Solution()
+
+encoded = [1,2,3]
+first = 1
+print(f" {encoded = } | {first = } | {sol.decode(encoded, first) = }")
+
+
+encoded = [6,2,7,3]
+first = 4
+print(f" {encoded = } | {first = } | {sol.decode(encoded, first) = }")
