2aedeaa78fddaefa6f5aa55098bdea2a566ec5d1
Ali Anwar <greenywish@gmail.com>
2025-10-14 01:54:33 +0530

    Solve Leetcode Problem 2273. Find Resultant Array After Removing Anagrams

---
+++ string/find_resultant_array_after_removing_anagrams_2273.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        result = [words[0]]
+        n = len(words)
+        for i in range(1,n):
+            if sorted(words[i]) != sorted(words[i-1]):
+                result.append(words[i])
+        return result
+
+
+sol = Solution()
+
+words = ["abba","baba","bbaa","cd","cd"]
+print(f"{words = } {sol.removeAnagrams(words) = }")
