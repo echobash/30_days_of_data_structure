613443fe5431a6d89fbc19f167688feb272c2e82
Ali Anwar <greenywish@gmail.com>
2026-01-02 00:09:01 +0530

    Solve Leetcode Problem 933. Number of Recent Calls with TLE

--- design/number_of_recent_calls_933.py
+++ design/number_of_recent_calls_933.py
@@ -1,7 +1,5 @@
 class RecentCounter:
     def getRequestCount(self, requests, start, end):
-        # start = 2
-        # end = 3002
         n = len(requests)
 
         i = 0
@@ -12,17 +10,9 @@ class RecentCounter:
 
         j = 0
         while j <= n - 1:
-            if requests[j] <= end:
+            if requests[j] >= end:
                 break
             j += 1
-
-        # j = n - 1
-        # while j >= 0:
-        #     if requests[i] <= end:
-        #         break
-        #     j -= 1
-
-        print(requests, start, end, i, j)
 
         return j - i + 1
 
