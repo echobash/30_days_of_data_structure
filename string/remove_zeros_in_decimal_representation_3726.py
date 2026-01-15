23a1ab9c31b6149b1102a8e6e7930762c56a636d
Ali Anwar <greenywish@gmail.com>
2026-01-16 00:14:08 +0530

    Solve Leetcode Problem 3726. Remove Zeros in Decimal Representation

---
+++ string/remove_zeros_in_decimal_representation_3726.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeZeros(self, n: int) -> int:
+        num = str(n)
+        result = []
+        for char in num:
+            if char != '0':
+                result.append(char)
+        return int("".join(result))
+
+
+sol = Solution()
+
+n = 1020030
+print(f"{n = } {sol.removeZeros(n) = }")
+
+n = 1
+print(f"{n = } {sol.removeZeros(n) = }")
